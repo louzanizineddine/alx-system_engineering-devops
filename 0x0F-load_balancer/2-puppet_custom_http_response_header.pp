3b838f65c05d2b5df9428ef7b46d900a4e26568d
louzanizineddine <louzanizineddine@gmail.com>
2023-12-05 00:02:14 +0100

    task 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,14 @@
+# Use Puppet to automate the task of creating a custom HTTP header response
+exec {'update':
+  command => '/usr/bin/apt-get update',
+}
+-> package {'nginx':
+  ensure => 'present',
+}
+-> exec { 'insert_header_line':
+  command => "sed -i '47i\        add_header X-Served-By \$hostname;' /etc/nginx/sites-enabled/default",
+  path    => ['/bin', '/usr/bin'],
+}
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
+
