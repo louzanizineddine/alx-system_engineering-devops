34c3ced1e680d0e8301f37d3faa03292a3098247
louzanizineddine <louzanizineddine@gmail.com>
2024-01-16 23:48:37 +0100

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Get the number of subs on reddit"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Get the number of subs on reddit"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'zineddine 1.0'}
+    res = requests.get(url, headers=headers)
+    if res.status_code == 200:
+        data = res.json()
+        len = data.get('data').get('subscribers')
+        return len     
+    else:
+        return 0
